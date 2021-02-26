2b1c6747a248a01de0ff7d4ed4878db294fd426c
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-26 23:54:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -41,7 +41,7 @@ def get_english_meaning(file_path, japanese_emoticon)
    #   binding.pry 
       english_meaning = meaning
     else
-      p english_meaning
+      print english_meaning
     end 
   end 
   # binding.pry 
